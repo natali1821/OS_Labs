5bca5f8e6fb63c9dc031a56807ebb0db6d058da1
natali1821 <ermolina_natali@bk.ru>
2022-12-12 22:41:03 +0300

    [second]: receiving program

---
+++ lab_4/second.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/sem.h>
+
+#define SHM_NAME "shmem_1"
+#define SHM_SIZE 64
+
+int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+
+	struct sembuf lock = {0, -1, 0};
+	struct sembuf unlock = {0, 1, 0};
+
+	struct timespec ts;
+
+	key_t key = ftok(SHM_NAME, 1);
+
+	int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666); 
+	if (shmid == -1) {
+		fprintf(stderr, "[first] shmget: %s(%d)\n", strerror(errno), errno);
+		exit(-1);
+	}
+
+	int semid = semget(key, 1, IPC_CREAT | 0666);
+	if (semid == -1) {
+		fprintf(stderr, "[first] semget: %s(%d)\n", strerror(errno), errno);
+		exit(-1);
+	}
+	
+	char* shm_ptr = shmat(shmid, NULL, 0);
+	if (!shm_ptr) {
+		printf("[second] Memory attach error\n");
+		exit(-1);
+	}
+
+	semop(semid, &lock, 1);
+	clock_gettime(CLOCK_REALTIME, &ts);
+	struct tm* curr = localtime(&ts.tv_sec);
+	double sec_ns = (double)curr->tm_sec + ((double)ts.tv_nsec / 1000000000.);
+	printf("[second] time: %2d:%2d:%.3lf pid: %d\n", curr->tm_hour, curr->tm_min, sec_ns, getpid());
+	printf("%s", shm_ptr);
+	semop(semid, &unlock, 1);
+
+	shmdt(shm_ptr);	
+
+	return 0;
+}
